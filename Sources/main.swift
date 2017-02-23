781861ee23f6818032c875144573247d9b44b480
Michael Bates <mklbtz@gmail.com>
2017-02-22 21:33:23 -0500

    add dummy commands

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,17 @@
-print("Hello, world!")
+import Commander
+
+let main = Group {
+  $0.command("add") { (title: String) in
+    print("Added task: 1. \(title)")
+  }
+  $0.command("rm") { (id: Int) in
+    if id == 1 {
+      print("Removed task 1.")
+    } else {
+      print("No task 1.")
+    }
+  }
+}
+
+main.run()
+
