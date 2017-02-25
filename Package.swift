9334be478cc3a144157791b1e5119ccd543d410b
Michael Bates <mklbtz@gmail.com>
2017-02-25 18:35:13 -0500

    change project name to Todo

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "tools",
+    name: "Todo",
     dependencies: [
         .Package(url: "https://github.com/kylef/Commander.git",
                  majorVersion: 0, minor: 6),
     ]
 )
+// package.products << Product(name: "Todo", type: .Library(.Dynamic), modules: "todo")
