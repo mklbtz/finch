05ee58c4c7da0cc0156c4e88761ded1a2ef0f555
Michael Bates <mklbtz@gmail.com>
2017-02-25 20:48:56 -0500

    remove comments and extra spacing

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Todo",
     dependencies: [
-        .Package(url: "https://github.com/kylef/Commander.git",
-                 majorVersion: 0, minor: 6),
+      .Package(url: "https://github.com/kylef/Commander.git",
+        majorVersion: 0, minor: 6),
     ]
 )
-// package.products << Product(name: "Todo", type: .Library(.Dynamic), modules: "todo")
