921aa30c54a48471fbb5446b2c928d92401c71c5
Michael Bates <mklbtz@gmail.com>
2017-02-25 20:22:23 -0500

    move private methods to extension

--- Sources/data_manager.swift
+++ Sources/data_manager.swift
@@ -3,26 +3,33 @@ import Foundation
 public struct DataManager {
   public init() {}
 
-  public func read() throws -> Data {
+  public
+  func read() throws -> Data {
     guard let data = fileManager.contents(atPath: dataPath)
     else { throw "Could not read file \(dataPath)" }
     return data
     }
 
-  public func write(data: Data) throws {
+  public
+  func write(data: Data) throws {
     guard fileManager.createFile(atPath: dataPath, contents: data)
     else { throw "Could not write data (\(data)) to file \(dataPath)" }
   }
+}
 
-  private var dataPath: String {
+extension DataManager {
+  fileprivate
+  var dataPath: String {
     return currentDirectory + "/.todo"
   }
 
-  private var currentDirectory: String {
+  fileprivate
+  var currentDirectory: String {
     return fileManager.currentDirectoryPath
   }
 
-  private var fileManager: FileManager {
+  fileprivate
+  var fileManager: FileManager {
     return FileManager.default
   }
 }
