84cff676cb535003e53c5509bc5c627c700faf86
Michael Bates <mklbtz@gmail.com>
2017-02-25 21:25:07 -0500

    make DataManager path configurable

--- Sources/data_manager.swift
+++ Sources/data_manager.swift
@@ -1,35 +1,30 @@
 import Foundation
 
 public struct DataManager {
-  public init() {}
+  public var dataPath: String
+
+  public
+  init(dataPath: String = defaultDataPath) {
+    self.dataPath = dataPath
+  }
 
   public
   func read() throws -> Data {
-    guard let data = fileManager.contents(atPath: dataPath)
+    guard let data = FileManager.default.contents(atPath: dataPath)
     else { throw "Could not read file \(dataPath)" }
     return data
-    }
+  }
 
   public
   func write(data: Data) throws {
-    guard fileManager.createFile(atPath: dataPath, contents: data)
+    guard FileManager.default.createFile(atPath: dataPath, contents: data)
     else { throw "Could not write data (\(data)) to file \(dataPath)" }
   }
 }
 
 extension DataManager {
   fileprivate
-  var dataPath: String {
-    return currentDirectory + "/.todo"
-  }
-
-  fileprivate
-  var currentDirectory: String {
-    return fileManager.currentDirectoryPath
-  }
-
-  fileprivate
-  var fileManager: FileManager {
-    return FileManager.default
+  static var defaultDataPath: String {
+    return FileManager.default.currentDirectoryPath + "/.todo"
   }
 }
