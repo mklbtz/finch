8b8a1fd78d494abf7f6ccf65a1cb21ad88966265
Michael Bates <mklbtz@gmail.com>
2017-03-02 07:32:06 -0500

    Remove default paths from DataManager

--- Sources/data_manager.swift
+++ Sources/data_manager.swift
@@ -3,28 +3,14 @@ import Foundation
 public struct DataManager {
   public var dataPath: String
 
-  public
-  init(dataPath: String = defaultDataPath) {
-    self.dataPath = dataPath
-  }
-
-  public
-  func read() throws -> Data {
+  public func read() throws -> Data {
     guard let data = FileManager.default.contents(atPath: dataPath)
     else { throw "Could not read file \(dataPath)" }
     return data
   }
 
-  public
-  func write(data: Data) throws {
+  public func write(data: Data) throws {
     guard FileManager.default.createFile(atPath: dataPath, contents: data)
     else { throw "Could not write data (\(data)) to file \(dataPath)" }
-  }
-}
-
-extension DataManager {
-  fileprivate
-  static var defaultDataPath: String {
-    return FileManager.default.currentDirectoryPath + "/.todo"
   }
 }
