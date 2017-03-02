ec045ebc7aaf052cb3b7b17854891c72ab5d4924
Michael Bates <mklbtz@gmail.com>
2017-03-02 14:17:19 -0500

    move yaml commands into subgroup

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,14 +2,18 @@ import Commander
 import Foundation
 import Yams
 
-Group {
-  $0.command("add") { (title: String) in
+func readAllLines() -> AnyIterator<String> {
+  return AnyIterator { return readLine(strippingNewline: false) }
+}
+
+Group { root in
+  root.command("add") { (title: String) in
     let task = Task(id: 0, title: title)
     let yaml = try ObjectStore.default.write(objects: [task])
     print(yaml)
   }
 
-  $0.command("rm") { (id: Int) in
+  root.command("rm") { (id: Int) in
     if id == 1 {
       print("Removed task 1.")
     }
@@ -18,28 +22,23 @@ Group {
     }
   }
 
-  $0.command("ls") {
+  root.command("ls") {
     let objects: [Node] = try ObjectStore.default.read()
     for (index, node) in objects.enumerated() {
       print("\(index): \(node)")
     }
   }
 
-  $0.command("read") {
-    let file = DataFile(at: ObjectStore.defaultFilePath)
-    do {
-      let data = try file.read()
-      print("Read data:", data)
+  root.group("yaml") { group in
+    group.command("read") {
+      let file = YamlFile(at: ObjectStore.defaultFilePath)
+      print(try file.read())
     }
-    catch let error {
-      print(error)
-    }
-  }
 
-  $0.command("write") {
-    let file = DataFile(at: ObjectStore.defaultFilePath)
-    let data = Data(bytes: [1,2,3,4,5])
-    do { try file.write(data: data) }
-    catch let error { print(error) }
+    group.command("write") {
+      let file = YamlFile(at: ObjectStore.defaultFilePath)
+      let yaml = readAllLines().joined()
+      try file.write(yaml: yaml)
+    }
   }
 }.run()
