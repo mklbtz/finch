61071562ebd240255728f6c137c94c8de9c45f05
Michael Bates <mklbtz@gmail.com>
2017-03-02 07:31:14 -0500

    Fix indentation in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Todo",
-    dependencies: [
-      .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
-      .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0)
-    ]
+  name: "Todo",
+  dependencies: [
+    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
+    .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0)
+  ]
 )
