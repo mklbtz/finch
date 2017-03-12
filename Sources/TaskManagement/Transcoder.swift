a108fcd44d7bccf428d0fa7d8ab61b698d8e9582
Michael Bates <mklbtz@gmail.com>
2017-03-12 00:29:52 -0500

    Add builders for common transcoder types.
    - Tasks to Yaml String
    - String to Data

--- Sources/TaskManagement/Transcoder.swift
+++ Sources/TaskManagement/Transcoder.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Yams
 
 public struct Transcoder<Input, Output> {
   let encoder: (Input) throws -> Output
@@ -16,4 +17,42 @@ public struct Transcoder<Input, Output> {
 public func + <A,B,C>(lhs: Transcoder<A,B>, rhs: Transcoder<B,C>) -> Transcoder<A,C> {
   return .init(encoder: { (a: A) -> C in try rhs.encode(lhs.encode(a)) },
                decoder: { (c: C) -> A in try lhs.decode(rhs.decode(c)) })
+}
+
+extension Transcoder {
+  public static var taskToYaml: Transcoder<[Task], String> {
+    return taskToNodes + nodeListToNode + nodeToString
+  }
+
+  public static var stringToData: Transcoder<String, Data> {
+    return .init(encoder: { $0.data(using: .utf8) ?? Data() },
+                 decoder: { String(data: $0, encoding: .utf8) ?? "" })
+  }
+}
+
+// Implementation for coding between Tasks and Yaml Strings.
+extension Transcoder {
+  static var taskToNodes: Transcoder<[Task], [Node]> {
+    return .init(encoder: { try $0.map { task in try task.represented() } },
+                 decoder: { $0.flatMap(Task.init(from:)) })
+  }
+
+  static var nodeListToNode: Transcoder<[Node], Node> {
+    return .init(encoder: { try $0.represented() },
+                 decoder: { node in
+                   switch node {
+                   case .sequence(let list, _, _):
+                     return list
+                   case .scalar("null", _, _):
+                     return []
+                   default:
+                     return [node]
+                   }
+                 })
+  }
+
+  static var nodeToString: Transcoder<Node, String> {
+    return .init(encoder: { try Yams.dump(object: $0) },
+                 decoder: { try Yams.load(yaml: $0).represented() })
+  }
 }
