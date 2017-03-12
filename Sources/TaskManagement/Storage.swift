5a4f3d73c287f7b0e2cd4ca2d2c96543c9ffcdd6
Michael Bates <mklbtz@gmail.com>
2017-03-12 00:39:47 -0500

    Add Storage struct
    Combines a File wrapper with a Transcoder.

---
+++ Sources/TaskManagement/Storage.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct Storage<Stored> {
+  public let path: String
+  let transcoder: Transcoder<Stored, Data>
+
+  public init(atPath path: String, transcoder: Transcoder<Stored, Data>) {
+    self.path = path
+    self.transcoder = transcoder
+  }
+}
+
+extension Storage {
+  public func load() throws -> Stored {
+    return try transcoder.decode(file.load())
+  }
+
+  public func save(_ stored: Stored) throws {
+    return try file.save(transcoder.encode(stored))
+  }
+
+  var file: File { return File(atPath: path) }
+}
+
+public typealias TaskStorage = Storage<[Task]>
+
+public func taskStorage() -> TaskStorage {
+  return TaskStorage(atPath: ".todo", transcoder: .taskToYaml + .stringToData)
+}
+
+public typealias YamlStorage = Storage<String>
+
+public func yamlStorage(atPath path: String) -> YamlStorage {
+  return YamlStorage(atPath: path, transcoder: .stringToData)
+}
