a739ad6aac1f5059a7a5aed173fc62ad509293a7
Michael Bates <mklbtz@gmail.com>
2017-03-12 00:40:50 -0500

    Fixup main to use new structs

--- Sources/todo/main.swift
+++ Sources/todo/main.swift
@@ -11,51 +11,54 @@ DefaultableGroup {
   let root = $0 as! DefaultableGroup
 
   root.defaultCommand {
-    let taskList = try taskListFile.read()
+    let taskList = try taskStorage().load()
     for task in taskList {
       TaskFormatter(for: task).print()
     }
   }
   root.command("add") { (title: String) in
-    var taskList = try taskListFile.read()
+    var taskList = try taskStorage().load()
     let id = taskList.nextId()
     let task = Task(id: id, title: title)
     taskList.append(task)
-    try taskListFile.write(taskList)
+    try taskStorage().save(taskList)
     TaskFormatter(for: task).print()
   }
 
   root.command("rm") { (id: Int) in
-    var taskList = try taskListFile.read()
+    var taskList = try taskStorage().load()
     let task = try taskList.remove(id: id)
-    try taskListFile.write(taskList)
+    try taskStorage().save(taskList)
     TaskFormatter(for: task).print()
   }
 
   root.command("ls") {
-    let taskList = try taskListFile.read()
+    let taskList = try taskStorage().load()
     for task in taskList {
       TaskFormatter(for: task).print()
     }
   }
 
   root.command("done") { (id: Int) in
-    var taskList = try taskListFile.read()
+    var taskList = try taskStorage().load()
     try taskList.update(id: id) { task in
       task.done = true
       TaskFormatter(for: task).print()
     }
-    try taskListFile.write(taskList)
+    try taskStorage().save(taskList)
   }
 
-  root.group("yaml") { group in
+  root.group("file") { group in
+    var yamlFile: YamlStorage {
+      return yamlStorage(atPath: ".todo")
+    }
     group.command("read") {
-      try todoYamlFile.read().print()
+      try yamlFile.load().print()
     }
 
     group.command("write") {
-      let yaml = readAllLines().joined()
-      try todoYamlFile.write(yaml)
+      let input = readAllLines().joined()
+      try yamlFile.save(input)
     }
   }
 }.run()
