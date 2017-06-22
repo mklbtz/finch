242f5f4886af5523822f7c60c785ab64f67d0dff
Michael Bates <mklbtz@gmail.com>
2017-06-22 03:10:49 -0400

    add DoCommand

---
+++ Sources/todo/DoCommand.swift
@@ -0,0 +1,41 @@
+import Commandant
+import Foundation
+import TaskManagement
+import Result
+
+struct DoCommand: CommandProtocol {
+  let verb = "do"
+  let function = "Complete tasks by ID"
+
+  func run(_ options: Options) -> Result<Void, String> {
+    return Result {
+      var manager = try TaskManager()
+      let update: ((Task) -> (Task)) throws -> ()
+
+      if options.markAll {
+        update = { try manager.update(by: $0) }
+      }
+      else {
+        update = { try manager.update(ids: options.ids, by: $0) }
+      }
+
+      try update { task in
+        var task = task
+        task.done = true
+        print(task)
+        return task
+      }
+    }
+  }
+
+  struct Options: OptionsProtocol {
+    let ids: [Int]
+    let markAll: Bool
+
+    static func evaluate(_ m: CommandMode) -> Result<Options, CommandantError<String>> {
+      return curry(Options.init)
+        <*> m <| Argument<[Int]>(usage: "A list of task IDs")
+        <*> m <| Switch(flag: "a", key: "all", usage: "Complete all tasks")
+    }
+  }
+}
