6bc745cc63528bb0b65fe23811de88e21946d132
Michael Bates <mklbtz@gmail.com>
2017-06-22 02:30:52 -0400

    add List Command

---
+++ Sources/todo/ListCommand.swift
@@ -0,0 +1,30 @@
+import Commandant
+import Foundation
+import TaskManagement
+import Result
+
+struct ListCommand: CommandProtocol {
+  let verb = "ls"
+  let function = "List outstanding tasks"
+
+  func run(_ options: Options) -> Result<Void, String> {
+    return Result {
+      let manager = try TaskManager()
+      let tasks = options.showAll ? manager.all : manager.outstanding
+      tasks.forEach { print($0) }
+    }
+  }
+
+  struct Options: OptionsProtocol {
+    let showAll: Bool
+
+    static func evaluate(_ m: CommandMode) -> Result<Options, CommandantError<String>> {
+      return Options.init <*> m <| all
+    }
+
+    static var all: Option<Bool> {
+      return Option(key: "all", defaultValue: false, usage: "Include completed tasks")
+    }
+  }
+}
+
