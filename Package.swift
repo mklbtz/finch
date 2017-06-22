72a926f8746aa2a8da0e7fb1f9561989e1b19dea
Michael Bates <mklbtz@gmail.com>
2017-06-22 01:24:28 -0400

    update to swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,13 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
   name: "todo",
-  targets: [
-    Target(name: "todo", dependencies: ["TaskManagement"]),
-    Target(name: "TaskManagement"),
+  products: [
+    .executable(name: "todo", targets: ["todo"]),
+    .library(name: "TaskManagement", targets: ["TaskManagement"])
   ],
   dependencies: [
-    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
-  ]
+    .package(url: "https://github.com/kylef/Commander.git", from: "0.0.0"),
+    .package(url: "https://github.com/Carthage/Commandant.git", .branch("master")),
+  ],
+  targets: [
+    .target(name: "todo", dependencies: ["TaskManagement", "Commandant", "Commander"]),
+    .target(name: "TaskManagement"),
+  ],
+  swiftLanguageVersions: [3, 4]
 )
