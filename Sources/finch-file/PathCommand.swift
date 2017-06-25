2645ddb7cece10f53e9166b69d0b2e215ebfa2fe
Michael Bates <mklbtz@gmail.com>
2017-06-25 18:02:51 -0400

    Remove use of global functions
    This should make the Commands more testable, while keeping the same
    level of efficiency.

--- Sources/finch-file/PathCommand.swift
+++ Sources/finch-file/PathCommand.swift
@@ -1,14 +1,20 @@
 import Commandant
 import Result
+import TaskManagement
 
 struct PathCommand: CommandProtocol {
   typealias Options = NoOptions<String>
 
   let verb = "path"
   let function = "Print path to task storage file"
+  var fileStorage: () -> Storage<String>
+
+  init(with storage: @autoclosure @escaping () -> Storage<String>) {
+    fileStorage = storage
+  }
 
   func run(_ options: Options) -> Result<Void, String> {
-    print(FileStorage().path)
+    print(fileStorage().path)
     return .success(())
   }
 }
